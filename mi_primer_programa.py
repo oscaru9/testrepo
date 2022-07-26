be867b96470390764fb50e87c3ea071268c821f3
oscaru9 <110054619+oscaru9@users.noreply.github.com>
2022-07-26 12:41:57 -0500

    Add files via upload
    New file

---
+++ mi_primer_programa.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 13:14:59 2020
+
+@author: Oscar_Urzua
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+A=np.array([[1,2,3],[2,3,4]])
+plt.figure(1)
+plt.pcolor(A)
